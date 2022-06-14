95fbc7c07486161bbe26bdb28ee502524aacf463
Dan <veras_dan@hotmail.com>
2022-06-14 01:37:40 -0300

    :wrench: Mudança em arquivos de configuração

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['client-management-project.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'client-management-project.herokuapp.com', ]
 
 
 # Application definition
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    'assets',
+]
 
 MEDIA_URL = '/media/'
 
