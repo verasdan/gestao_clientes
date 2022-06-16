d58589050387772bf20fc1c81bef27241682d404
Dan <veras_dan@hotmail.com>
2022-06-16 16:38:20 -0300

    🧪 :test_tube: test: Criando novo teste

--- gestao_clientes/asgi.py
+++ gestao_clientes/asgi.py
@@ -8,13 +8,13 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-from whitenoise import WhiteNoise
-from gestao_clientes import MyWSGIApp
+# from whitenoise import WhiteNoise
+# from gestao_clientes import MyWSGIApp
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestao_clientes.settings')
 
 application = get_asgi_application()
-application = MyWSGIApp()
-application = WhiteNoise(application, root="assets")
+# application = MyWSGIApp()
+# application = WhiteNoise(application, root="assets")
--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -138,4 +138,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
